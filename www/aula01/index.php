7781c95e172ca96aadc60b098bc3a47668bbdd2e
crissbarichello <cristianobari2010@gmail.com>
2026-06-16 21:46:32 -0300

    Aula1 teste de php

---
+++ www/aula01/index.php
@@ -0,0 +1,17 @@
+<?php 
+echo "<h1>Tecnologia...</h1>"; 
+
+echo phpversion();
+
+
+ECHO 'Hello World!<br>';
+echo 'Hello World!<br>';
+
+$txt = "W3Schools.com";
+echo "I love $txt!</br>";
+
+echo "I love $txt!</br>";
+echo "I love $txt!</br>";
+
+
+?>
