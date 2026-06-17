db4e2f2785530c30ac6d6aec0cee6fa2414148dd
crissbarichello <cristianobari2010@gmail.com>
2026-06-16 22:20:26 -0300

    teste projetos

---
+++ www/projeto01/welcome1.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+
+<?php list($a, $b, $c) = [1, 2, 3];
+echo "$a is " . $a . "<br>";
+echo "$b is " . $b . "<br>";
+echo "$c is " . $c . "<br>"; ?>
+
+</body>
+</html>
